8e42ef20473e5a5716179f779aa31449b3617c50
neelesh kumar <neeleshmasula@gmail.com>
2024-08-21 09:21:25 +0530

    header added in master branch

--- src/shoppingCart/Header.js
+++ src/shoppingCart/Header.js
@@ -4,16 +4,7 @@ import { FaShoppingCart } from "react-icons/fa";
 const Header = () => {
   return (
     <header className="header">
-      <h2>ShoppingCart</h2>
-      <form>
-        <input type="text" placeholder="Search a Product" />
-      </form>
-      <div className="dropdowns">
-        <FaShoppingCart />
-        <select>
-          <option value="fruit">11</option>
-        </select>
-      </div>
+      <h1>header added in master branch</h1>
     </header>
   );
 };
