c95944858bff8f14fc4f122dc773471ea189a973
neelesh kumar <neeleshmasula@gmail.com>
2024-08-21 09:13:51 +0530

    another navigationhas been added

--- navigation.js
+++ navigation.js
@@ -4,6 +4,9 @@ const navigation = () => {
   return (
     <nav>
       <h1>vigation</h1>
+      <h3>
+        After making changes in master now another new navigation has been added
+      </h3>
     </nav>
   );
 };
